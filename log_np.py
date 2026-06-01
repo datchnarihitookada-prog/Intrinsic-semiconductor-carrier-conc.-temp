703d103f24b9a05ebbc606dc7b5f713c5c8a2ff0
datchnarihitookada-prog <datchnarihitookada@gmail.com>
2026-06-01 23:07:19 +0900

    Update log_np.py

--- log_np.py
+++ log_np.py
@@ -91,7 +91,7 @@ ax.plot(
 ax.set_xlabel("1/T (K$^{-1}$)", fontsize=14)
 ax.set_ylabel("log p (cm$^{-3}$)", fontsize=14)
 
-ax.set_xlim(0, 0.012)
+ax.set_xlim(0, 0.02)
 ax.set_ylim(0, 23) 
 ax.tick_params(direction="in", labelsize=12)
 
